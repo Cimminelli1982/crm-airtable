6326ad9fc3140ed053fe6a98bb2877c4877a6a3e
Simone Cimminelli <simonecimminelli@Simones-MacBook-Air.local>
2024-12-26 18:16:10 +0000

    Shorten airtable elements

--- functions/contact-records.js
+++ functions/contact-records.js
@@ -77,7 +77,6 @@ exports.handler = async (event, context) => {
       { airtableField: "Last contact", displayName: "Last contact" },
       { airtableField: "Contact Airtable ID", displayName: "Contact Airtable ID" },
       { airtableField: "HubSpot ID", displayName: "HubSpot ID" },
-      { airtableField: "Keep in Touch", displayName: "Keep in touch" },
       { airtableField: "Main Category", displayName: "Main category" },
       { airtableField: "Job Title", displayName: "Job title" },
       { airtableField: "Company (string from Hubspot)", displayName: "Company" },
@@ -85,13 +84,6 @@ exports.handler = async (event, context) => {
       { airtableField: "Mobile phone number", displayName: "Mobile phone number" },
       { airtableField: "Phone number (from timelines)", displayName: "Phone number (from timelines)" },
       { airtableField: "Primary email", displayName: "Primary email" },
-      { airtableField: "Linkedin adjusted", displayName: "Linkedin" },
-      { airtableField: "City", displayName: "City" },
-      { airtableField: "Rating", displayName: "Rating" },
-      { airtableField: "Next Keep in touch", displayName: "Next Birthday" },
-      { airtableField: "Birthday wishes", displayName: "Birthday wishes" },
-      { airtableField: "Christmas wishes", displayName: "Christmas wishes" },
-      { airtableField: "Easter wishes", displayName: "Easter wishes" },
     ];
 
     // Fetch Airtable Records
